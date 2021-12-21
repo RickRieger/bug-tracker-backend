803793e04f79c9c6b48becea4302ab6d56ad8922
Rick Rieger <rickrieger36251@gmail.com>
2021-12-21 09:51:28 -0500

    test2

--- routes/utils/multerMiddleware.js
+++ routes/utils/multerMiddleware.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+    cb(null, '/home/ubuntu/bug-tracker-backend/uploads');
+    ///home/ubuntu/bug-tracker-backend/uploads
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
