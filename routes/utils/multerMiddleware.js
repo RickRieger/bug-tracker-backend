56dd258935f8a11670199715894203bd156329e6
Rick Rieger <rickrieger36251@gmail.com>
2021-12-21 09:46:50 -0500

    test

--- routes/utils/multerMiddleware.js
+++ routes/utils/multerMiddleware.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../../uploads'));
+    cb(null, './uploads');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
